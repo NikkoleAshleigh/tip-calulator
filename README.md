a7a493aea39434df0a6c4e89ec6c8c44993b6900
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-10-18 14:28:12 -0400

    added in necessary comments to talk through the thought process, updated markdown in README and tried to add in more logic to take in second tip value

--- README.md
+++ README.md
@@ -3,7 +3,20 @@
 
 # Tip Calulator
 
-Program in python that calculates a tip
+Program in python3 that calculates a tip with the total bill. 
+Once completed The user may enter in a second tip amount.
 
+______________________________________
+
+## How it works
+
+In the terminal run: ``python project_tip_calculator.py``
+- User enters the amount of the bill and how many ways the bill will be split
+- User enters the value of the tip they wish to add to the amount of the bill
+- The sales tax will then be configured to then print the total of either how much each person will spend if the bill was split or how much the total comes to if a single user were paying
+- User then enters whether they wish to add another tip or not
+    - If yes, the program will then re-calculate the bill with the new tip added
+    - If not, the previous total amount will be printed again
+______________________________________
 
 
--- project_tip_calculator.py
+++ project_tip_calculator.py
@@ -1,31 +1,58 @@
-def tip_calculator():
+# I need to store the user input to get the value of the bill and
+# how many ways the bill will be split
 
-    the_bill = float(input('What is the bill total? '))
+# def  get_the_bill():
+the_bill = float(input('What is the bill total? '))
 
-    ways_to_split = int(input('How many people will be paying? '))
-
-    tip_amount = float(input('How much would you like to leave as a tip? '))
-    if tip_amount <= 5:
-        print('Ouch, that\'s lousy.')
-    elif tip_amount <= 10:
-        print('Were there any complaints with your service? ')
-    elif tip_amount <= 20:
-        print('I hope you enjoyed dining with us!')
-    else:
-        print('It\'s been an absolute pleasure serving you, and we hope to see you soon!' )
-
-    tip = (the_bill * (0.01 * tip_amount))
-    
-    sales_tax = (the_bill * 0.10)
-    # print(sales_tax)
-    total_bill = (the_bill + tip + sales_tax / ways_to_split)
-
-    if ways_to_split >= 2:
-        print(f'Each person should pay ${total_bill}')
-    else:
-        print(f'Your total comes to: ${total_bill}')
-
-tip_calculator()
+ways_to_split = int(input('How many people will be paying? '))
 
 
+# I need to store the user input to get the value of the tip they
+# wish to add to the bill (also added a little flare with the amount)
+
+# def the_tip():
+tip_amount = float(input('How much would you like to leave as a tip? '))
+if tip_amount <= 5:
+    print('Ouch, that\'s lousy.')
+elif tip_amount <= 10:
+    print('Were there any complaints with your service? ')
+elif tip_amount <= 20:
+    print('I hope you enjoyed dining with us!')
+else:
+    print('It\'s been an absolute pleasure serving you, and we hope to see you soon!' )
+
+tip = (the_bill * (0.01 * tip_amount))
+# the_tip()
+
+# def total_bill()
+
+# I need to calculate the 10% sales tax in with the bill 
+# in order to get the final total 
+
+sales_tax = (the_bill * 0.10)
+# print(sales_tax)
+total_bill = (the_bill + tip + sales_tax / ways_to_split)
+
+if ways_to_split >= 2:
+    print(f'Each person should pay ${total_bill}')
+else:
+    print(f'Your total comes to: ${total_bill}')
+
+# total_bill()
+
+# After all is said and done, I nee to ask the user if they are going 
+# to be adding a second bill and run through the calulations again
+
+# def adding_second_tip()
+another_tip = bool(input('Would you like to add another tip? '))
+if another_tip == 'Yes':
+    second_tip = float(input('How much would you like to leave as a tip? '))
+    # I'm not able to get past asking if they want to add another tip 
+    second_total = total_bill + second_tip
+    print(second_total)
+else:
+    print(total_bill)
+# adding_second_tip()
+
+print('We hope to have your service again and have a blessed day! Thank you!')
 
