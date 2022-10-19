08b06f528a300392180fe0fa63ad880b1271d6a1
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-10-19 14:01:14 -0400

    made a few updates to comments

--- README.md
+++ README.md
@@ -17,5 +17,36 @@ In the terminal run: ``python project_tip_calculator.py``
     - If yes, the program will then re-calculate the bill with the new tip added
     - If not, the previous total amount will be printed again
 
+## Cases to test my app:
 
-
+### A meal for 1
+Input:
+- Food costs $15
+- 1 person paying
+- 20% tip
+Expected output:
+- ``Total bill: 19.50``
+### A feast to remember
+Input:
+- Food costs $25000000
+- 3 person paying
+- 31% tip
+Expected output:
+- ``Total bill: $35,250,000.00``
+- ``Each person should pay $11,750,000.00``
+### No tip
+Input:
+- Food costs $78.99
+- 6 person paying
+- 0 tip
+Expected output:
+- ``Total bill: $86.89``
+- ``Each person should pay $14.48``
+### Sharing the bill among many
+Input:
+- Food costs $5000
+- 876 person paying
+- 12% tip
+Expected output:
+- ``Total bill: $6,100.00``
+- ``Each person should pay $11,750,000.00``
--- project_tip_calculator.py
+++ project_tip_calculator.py
@@ -1,8 +1,8 @@
 # I need to store the user input to get the value of the bill and
 # how many ways the bill will be split
 
-# def  get_the_bill():
-the_bill = float(input('What is the bill total? '))
+
+the_bill = float(input('What is the bill total? $'))
 
 ways_to_split = int(input('How many people will be paying? '))
 
@@ -10,49 +10,53 @@ ways_to_split = int(input('How many people will be paying? '))
 # I need to store the user input to get the value of the tip they
 # wish to add to the bill (also added a little flare with the amount)
 
-# def the_tip():
-tip_amount = float(input('How much would you like to leave as a tip? '))
-if tip_amount <= 5:
-    print('Ouch, that\'s lousy.')
-elif tip_amount <= 10:
-    print('Were there any complaints with your service? ')
-elif tip_amount <= 20:
-    print('I hope you enjoyed dining with us!')
+def the_tip():
+    tip_amount = float(input('How much would you like to leave as a tip? '))
+    if tip_amount <= 5:
+        print('Ouch, that\'s lousy.')
+    elif tip_amount <= 10:
+        print('Were there any complaints with your service? ')
+    elif tip_amount <= 20:
+        print('I hope you enjoyed dining with us!')
+    else:
+        print('It\'s been an absolute pleasure serving you, and we hope to see you soon!' )
+
+    tip = (the_bill * (0.01 * tip_amount))
+    return tip
+tip = the_tip()
+ 
+# Once the program has run, I need to ask the user if they are going to be 
+# adding a second bill and run through the calulations again. 
+# The reason I place this here is because I dont want to change the entire 
+# bill amount and not give the correct amount in the end
+
+another_tip = input('Would you like to add another tip? ')
+if another_tip == 'Yes':
+    tip_new = tip + the_tip()
 else:
-    print('It\'s been an absolute pleasure serving you, and we hope to see you soon!' )
-
-tip = (the_bill * (0.01 * tip_amount))
-# the_tip()
-
-# def total_bill()
+    print('We hope to have your service again and have a blessed day! Thank you!')
 
 # I need to calculate the 10% sales tax in with the bill 
-# in order to get the final total 
+# in order to get the final total
 
 sales_tax = (the_bill * 0.10)
 # print(sales_tax)
-total_bill = (the_bill + tip + sales_tax / ways_to_split)
+total_bill = ((the_bill + tip + sales_tax) / ways_to_split)
 
+# I need to round the bill 
+final_bill = round(total_bill, 2)
+
+# I want to give different feedback based on if the bill was split or not
 if ways_to_split >= 2:
-    print(f'Each person should pay ${total_bill}')
+    print(f'Each person should pay ${final_bill}')
 else:
-    print(f'Your total comes to: ${total_bill}')
+    print(f'Your total comes to: ${final_bill}')
 
-# total_bill()
+# After all is said and done, I need to ask the user if they are going 
+# to be adding a second bill and run through the calulations again The code 
+# is on lines 31-35. Explanation for placement given there as well.
 
-# After all is said and done, I nee to ask the user if they are going 
-# to be adding a second bill and run through the calulations again
 
-# def adding_second_tip()
-another_tip = bool(input('Would you like to add another tip? '))
-if another_tip == 'Yes':
-    second_tip = float(input('How much would you like to leave as a tip? '))
-    # I'm not able to get past asking if they want to add another tip 
-    second_total = total_bill + second_tip
-    print(second_total)
-else:
-    print(total_bill)
-# adding_second_tip()
 
-print('We hope to have your service again and have a blessed day! Thank you!')
+
 
